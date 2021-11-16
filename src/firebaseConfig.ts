f861a84be17e786376b43e43343578a254071a4f
Joshua Cunningham <86850223+jcnghm@users.noreply.github.com>
2021-11-15 17:57:23 -0800

    firebase update and API update

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyANNLkZC8dF6MvoQK3npIc6Bqux0E9hf-Y",
-    authDomain: "the-dugout-4d519.firebaseapp.com",
-    projectId: "the-dugout-4d519",
-    storageBucket: "the-dugout-4d519.appspot.com",
-    messagingSenderId: "441397845520",
-    appId: "1:441397845520:web:4d2e2485d3d6be0a447495"
-  };
+  apiKey: "AIzaSyCHANEf1tPKUP6kcfXCO6GS_F-K6XJDBpw",
+  authDomain: "my-dugout.firebaseapp.com",
+  projectId: "my-dugout",
+  storageBucket: "my-dugout.appspot.com",
+  messagingSenderId: "948923706228",
+  appId: "1:948923706228:web:ccde141ab60e0a30341a21"
+};
